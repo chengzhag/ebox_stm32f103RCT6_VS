b9582668163f23b51236e9292df4eeca49921ac1
pidan1231239 <850576570@qq.com>
2017-05-31 21:35:45 +0800

    添加sky::PID积分分离式pid

--- ebox_stm32f103RCT6_VS/ebox_stm32f103RCT6_VS/common/edriver/PID.hpp
+++ ebox_stm32f103RCT6_VS/ebox_stm32f103RCT6_VS/common/edriver/PID.hpp
@@ -3,6 +3,7 @@
 
 namespace greg {
 	class PID {
+	protected:
 		float last_error;
 		float last_output;
 		float set_point;
@@ -71,5 +72,78 @@ namespace greg {
 		void reset();
 	};
 };
+
+namespace sky
+{
+	class PID:public greg::PID
+	{
+		float iSeperateThres;
+		bool isInISeperateThres;
+	public:
+		//积分分离式PID
+		//误差小于iSeperateThres时，先清零积分项，开启积分控制
+		//大于时，关闭积分控制
+		PID() :
+			greg::PID(),
+			iSeperateThres(0),
+			isInISeperateThres(true)
+		{
+
+		}
+
+		//设置积分分离式PID误差阈值
+		void setISeperateThres(float iSeperateThres)
+		{
+			this->iSeperateThres = iSeperateThres;
+		}
+
+		float refresh(const float &feedback_input)
+		{
+			error = set_point - feedback_input;
+			if (error >= error_threshold || error <= -error_threshold) {
+				last_output = Kp*error+ Kd*(error - last_error);
+				if (error > -iSeperateThres&&error < iSeperateThres)
+				{
+					if (!isInISeperateThres)
+					{
+						integral = 0;
+						isInISeperateThres = true;
+					}
+					last_output += Ki*integral;
+				}
+				else
+				{
+					if (isInISeperateThres)
+					{
+						isInISeperateThres = false;
+					}
+				}
+
+				if (last_output > output_upper_limit) {
+					last_output = output_upper_limit;
+					if (integral / error < 0.f) {
+						integral += (error + last_error) / 2.f;
+						last_error = error;
+					}
+					return output_upper_limit;
+				}
+				if (last_output < output_lower_limit) {
+					if (integral / error < 0.f) {
+						integral += (error + last_error) / 2.f;
+						last_error = error;
+					}
+					last_output = output_lower_limit;
+					return output_lower_limit;
+				}
+				integral += (error + last_error) / 2.f;
+				last_error = error;
+			}
+			return last_output;
+		}
+
+
+
+	};
+}
 
 #endif
