5602f7793f5786bdc51fac7a2144bbb9f715d80a
pidan1231239 <850576570@qq.com>
2017-06-23 17:48:11 +0800

    完成oled_i2c驱动。TODO：添加printf函数

--- ebox_stm32f103RCT6_VS/ebox_stm32f103RCT6_VS/user/main.cpp
+++ ebox_stm32f103RCT6_VS/ebox_stm32f103RCT6_VS/user/main.cpp
@@ -1,43 +1,36 @@
 #include "ebox.h"
 #include "led.h"
-#include "drv8825.h"
+#include "oled_i2c.h"
+#include "Button.h"
 
-Led led(&PC13, 1);
-DRV8825 step(&PB8, &PC9);
+OLEDI2C oled(&i2c2);
 
 void setup()
 {
 	ebox_init();
 	uart1.begin(115200);
-	led.begin();
-	step.begin();
+	oled.begin();
 }
 int main(void)
 {
 	setup();
-	float pct = 0, increase = 0.001;
+	int num = 1512;
 	while (1)
 	{
-		pct += increase;
-		if (pct >= 0.03 || pct <= -0.03)
-		{
-			increase = -increase;
-		}
-		step.setPct(pct);
-
-
-		led.toggle();
-		uart1.printf("%f\r\n", step.getPct());
-		delay_ms(100);
-		if (millis()>5000)
-		{
-			break;
-		}
+		oled.show_string(0, 0, "ok!!!!", 1);
+		delay_ms(1000);
+		oled.show_string(0, 0, "ok!!!!", 2);
+		delay_ms(1000);
+		oled.show_chinese(0, 2, 0);
+		delay_ms(1000);
+		//oled.printf(0, 4, 1,"ok!!%d", num);
+		//delay_ms(1000);
+		oled.draw_bmp(0, 0, 128, 8, (unsigned char *)BMP1);
+		delay_ms(1000);
+		oled.draw_bmp(0, 0, 128, 8, (unsigned char *)BMP2);
+		delay_ms(1000);
+		oled.clear();
 	}
-
-	//测试设置频率为0时的情况
-	step.setPct(0);
-	while (1);
 
 }
 
